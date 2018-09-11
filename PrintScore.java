00f96d351de0cfc4a2c9394a50fcdea400c55fdd
dingzhenhau <dingzhenhuakf.com>
2018-09-11 19:59:55 +0800

    java function

---
+++ PrintScore.java
@@ -0,0 +1,14 @@
+public class PrintScore{
+	public static void main(String[] args){
+		PrintScore a = new PrintScore();
+
+		int rSum;
+		
+		rSum=a.calcSum(7,79);
+		System.out.println("rSum= "+rSum);
+}
+	public int calcSum(int a, int b){
+		return (a+b);
+
+}
+}
